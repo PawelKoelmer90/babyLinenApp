edf0605ff154e30cc1fdac6f7129e019fb2ac948
Pawel Koelmer <pawel.koelmer@gmail.com>
2025-02-10 11:50:42 +0100

    Added missing isChecked

--- linenApp/src/components/custom/Checkbox.tsx
+++ linenApp/src/components/custom/Checkbox.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Checkbox = ({ isChecked, changeChecked }: Props) => {
   return (
     <>
-      <input type={"checkbox"} onClick={changeChecked} />
+      <input type={"checkbox"} onClick={changeChecked} checked={isChecked} />
     </>
   );
 };
